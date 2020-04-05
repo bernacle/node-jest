0d71f5d31d4d3c6bd97242e29ba35d6f83af2837
Bruno  Simplício <brunosimplicio@MacBook-Pro-de-Bruno.local>
2020-04-04 22:29:22 -0300

    Unit tests and integration tests using TDD

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,9 +1,24 @@
+const bcrypt = require("bcryptjs");
+
 module.exports = (sequilize, DataTypes) => {
-  const User = sequilize.define("User", {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password_hash: DataTypes.STRING,
-  });
+  const User = sequilize.define(
+    "User",
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
+    },
+    {
+      hooks: {
+        beforeSave: async (user) => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8);
+          }
+        },
+      },
+    }
+  );
 
   return User;
 };
--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
+
 module.exports = {
   host: "localhost",
   port: "32769",
-  username: "docker",
-  password: "docker",
-  database: "docker",
-  dialect: "postgres",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || "postgres",
+  storage: "./__tests__/database.sqlite",
   logging: false,
   define: {
     timestamps: true,
